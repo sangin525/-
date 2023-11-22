c5b3667b78bb3a9871693faea22f155eaa507855
Bobby <jxbkc@naver.com>
2023-11-22 13:03:37 +0900

    호텔소개

---
+++ SemiProject/src/main/java/kr/co/project/hotel/form/controller/FormController.java
@@ -0,0 +1,27 @@
+package kr.co.project.hotel.form.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/form/*")
+public class FormController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public FormController() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html; charset=utf-8");
+		
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	}
+
+}
---
+++ SemiProject/src/main/webapp/views/common/nav2.jsp
@@ -0,0 +1,14 @@
+
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+        <nav class="sidebarlist">
+          <div class="sidebar">
+            <h2 class="sidebartit">호텔소개</h2>
+            <ul class="menu">
+              <li><a href="HotelInfo.html">호텔개요</a></li>
+              <li><a href="HotelInfoPath.html">오시는 길</a></li>
+              <li><a href="Hotelrestaurant.html">호텔 식당</a></li>
+              <li><a href="HotelPool.html">호텔서비스</a></li>
+            </ul>
+          </div>
+        </nav>
